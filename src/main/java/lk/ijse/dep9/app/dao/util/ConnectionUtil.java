80958fbd4243c3d541dd6ce1aa68fc646930e3c7
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-12-20 16:03:48 +0530

    Finalize the structure of the DAO Layer

--- src/main/java/lk/ijse/dep9/app/dao/util/ConnectionUtil.java
+++ src/main/java/lk/ijse/dep9/app/dao/util/ConnectionUtil.java
@@ -6,11 +6,11 @@ public class ConnectionUtil {
 
     private static final ThreadLocal<Connection> threadLocal = new ThreadLocal<>();
 
-    public static void setConnection(Connection connection){
-        threadLocal.set(connection);
+    public static Connection getConnection() {
+        return threadLocal.get();
     }
 
-    public static Connection getConnection(){
-        return threadLocal.get();
+    public static void setConnection(Connection connection) {
+        threadLocal.set(connection);
     }
 }
